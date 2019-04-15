f0b422bc49ecc3a403b385f833c7fa9f2bdb0599
LAPTOP-E7U3H4P1\47009 <470091203@qq.com>
2019-04-16 00:21:28 +0800

    update

---
+++ src/main/java/com/learning/Test01.java
@@ -0,0 +1,18 @@
+package com.learning;
+
+/**
+ * ClassName:Test01
+ * Package:com.learning
+ * Description:
+ * Date:2019/4/15 20:55
+ * Author:1365102705
+ */
+public class Test01 {
+    public static void main(String[] args) throws Exception {
+        String str = "Hello";
+
+        ThreadCall threadCall = new ThreadCall();
+        threadCall.call();
+
+    }
+}
---
+++ src/main/java/com/learning/ThreadCall.java
@@ -0,0 +1,17 @@
+package com.learning;
+
+import java.util.concurrent.Callable;
+
+/**
+ * ClassName:ThreadCall
+ * Package:com.learning
+ * Description:
+ * Date:2019/4/15 21:58
+ * Author:13651027050
+ */
+public class ThreadCall implements Callable {
+    @Override
+    public Object call() throws Exception {
+        return null;
+    }
+}
