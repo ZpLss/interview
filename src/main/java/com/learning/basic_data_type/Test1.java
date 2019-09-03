5f0e7f23bddabf62e615b5d41006160b5fdbf546
470091203@qq.com <Zplss>
2019-09-03 17:29:51 +0800

    基本数据类型

---
+++ src/main/java/com/learning/basic_data_type/Test1.java
@@ -0,0 +1,26 @@
+package com.learning.basic_data_type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 基本数据类型
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        //可以定义泛型参数
+        List<Integer> list = new ArrayList<>();
+        //数据缓存
+        Integer num1 = 127;
+        Integer num2 = 127;
+        System.out.println(num1 == num2);
+        Integer num3 = 128;
+        Integer num4 = 128;
+        System.out.println(num3 == num4);//false
+        System.out.println(num3.equals(num4));//true
+
+
+
+    }
+
+}
