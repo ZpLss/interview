b9a49c397c3f3a0260a2017a1de48ae143bfd4a3
LAPTOP-E7U3H4P1\47009 <470091203@qq.com>
2020-12-14 22:31:57 +0800

    最新面试资料

--- src/main/java/com/learning/InterviewApplication.java
+++ src/main/java/com/learning/InterviewApplication.java
@@ -3,11 +3,27 @@ package com.learning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.FileReader;
+
 @SpringBootApplication
 public class InterviewApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InterviewApplication.class, args);
+	public static void main(String[] args) throws Exception {
+//		SpringApplication.run(InterviewApplication.class, args);
+		if("200".equals(null)){
+			System.out.println("123");
+		}
+
+
+		File file = new File("/d");
+		FileReader fileReader = null;
+		try {
+			fileReader.close();
+		}catch (Exception e){}
+		  throw new Exception("youwenti");
+
+
 	}
 
 }
